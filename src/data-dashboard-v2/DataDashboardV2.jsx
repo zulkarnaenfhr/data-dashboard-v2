62ee4983fe695d77153a4a97593ac252bf10bcac
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-03-01 12:31:20 +0700

    first commit

---
+++ src/data-dashboard-v2/DataDashboardV2.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Datadashboardv2 extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>masuk data v2</h1>
+            </div>
+        );
+    }
+}
+
+export default Datadashboardv2;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Datadashboardv2 from './data-dashboard-v2/DataDashboardV2';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Datadashboardv2 />
   </React.StrictMode>,
   document.getElementById('root')
 );
